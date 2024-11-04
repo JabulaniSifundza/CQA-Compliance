72d15cb9200369222d1abd2353862a3e7804ebd7
JabulaniSifundza <jjsifundza@gmail.com>
2024-11-04 15:52:20 -0600

    Index test

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def read_excel(file):
     df = pd.read_csv(file, skiprows=5)
     # print(df)
     st.write(df)
-    row_1 = df.loc["Cash Balance"]
+    row_1 = df.loc[0]
     st.write(row_1)
     
 
