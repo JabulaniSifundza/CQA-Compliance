7ea43f302dec41ca891a611a5c9bd885f5164478
JabulaniSifundza <jjsifundza@gmail.com>
2024-11-04 16:00:29 -0600

    Level 1 test

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def read_excel(file):
     # row_1 = df.loc[1]
     # st.write(row_1)
     st.write(df.index)
+    st.write(df.loc['level_1'])
     
 
 def portfolio_compliance_assistance(current_portfolio_value=1000000.00, current_cash_amount=1.00, long_postions_value=1000000.00, short_position_value=1000000.00):
