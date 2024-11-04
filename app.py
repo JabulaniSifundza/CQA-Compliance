5d70e8cbfb5177c72cc702c7aa397cdb1bbdc2c1
JabulaniSifundza <jjsifundza@gmail.com>
2024-11-04 15:35:17 -0600

    Row exploration

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ def read_excel(file):
     # Load the Excel file
     df = pd.read_csv(file, skiprows=5)
     row = df.iloc[0]
+    st.write(df)
+    st.write(row)
     st.write(row[1])
+    
     
 
 def portfolio_compliance_assistance(current_portfolio_value=1000000.00, current_cash_amount=1.00, long_postions_value=1000000.00, short_position_value=1000000.00):
