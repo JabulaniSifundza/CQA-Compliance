dee4a46bdecac082bb46ac8427fb66d64206eb71
JabulaniSifundza <jjsifundza@gmail.com>
2024-11-21 23:05:16 -0600

    Tab position fixing

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def portfolio_compliance_assistance(current_portfolio_value=1000000.00, current_
 
 def main():
     st.title("CQA Compliance Tool")
-    cash_dollar_neutrality, individual_position_value, upload_current_position_excel, filter_long_positions, filter_short_positions = st.tabs(["Cash and Dollar Neutrality", "Individual Position Value", "Current Position Filter (Long)", "Current Position Filter (Short)","Upload Position Spreadsheet"])
+    cash_dollar_neutrality, individual_position_value, filter_long_positions, filter_short_positions, upload_current_position_excel, = st.tabs(["Cash and Dollar Neutrality", "Individual Position Value", "Current Position Filter (Long)", "Current Position Filter (Short)", "Upload Position Spreadsheet"])
     with cash_dollar_neutrality:
         st.title("Cash and Dollar Neutrality")
         current_portfolio_value = st.number_input("Enter Portfolio Value Including Decimals 👇🏾", placeholder="Current Portfolio Value", key="current_portfolio_value", step=1., format="%.2f")
@@ -127,8 +127,8 @@ def main():
             portfolio_data = uploaded_file
             read_excel(portfolio_data)
     with filter_long_positions:
-        uploaded_file_containing_open_positions = st.file_uploader("Upload the CSV of Open Positions", type=["csv"], key="uploaded_file_current_open_positions_long")
-        uploaded_file_containing_long_model_positions = st.file_uploader("Upload the long positions from the model", type=["xlsx"], key="uploaded_file_recommended_open_positions_long")
+        uploaded_file_containing_open_positions = st.file_uploader("Upload the CSV of Open Positions", type=["csv"],key="current_open_positions_long")
+        uploaded_file_containing_long_model_positions = st.file_uploader("Upload the long positions from the model", type=["xlsx"],key="recommended_positions_long")
         if uploaded_file_containing_open_positions and uploaded_file_containing_long_model_positions:
             recommended_long_positions_df = pd.read_excel(uploaded_file_containing_long_model_positions)
             opened_long_positions_df = pd.read_csv(uploaded_file_containing_open_positions)
@@ -145,8 +145,8 @@ def main():
             for position in current_not_opened_long_positions_from_model['ticker_symbol']:
                 st.write(position)
     with filter_short_positions:
-        uploaded_file_containing_open_positions = st.file_uploader("Upload the CSV of Open Positions", type=["csv"], key="uploaded_file_current_open_positions_short")
-        uploaded_file_containing_short_model_positions = st.file_uploader("Upload the short positions from the model", type=["xlsx"], key="uploaded_file_recommended_open_positions_short")
+        uploaded_file_containing_open_positions = st.file_uploader("Upload the CSV of Open Positions", type=["csv"], key="current_open_positions_short")
+        uploaded_file_containing_short_model_positions = st.file_uploader("Upload the short positions from the model", type=["xlsx"], key="recommended_positions_short")
         if uploaded_file_containing_open_positions and uploaded_file_containing_short_model_positions:
             recommended_short_positions_df = pd.read_excel(uploaded_file_containing_short_model_positions)
             opened_short_positions_df = pd.read_csv(uploaded_file_containing_open_positions)
